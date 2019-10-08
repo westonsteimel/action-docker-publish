b5268269c0021391c1f55f0470155d480dad77eb
Weston Steimel <weston.steimel@gmail.com>
2019-10-08 23:43:54 +0100

    add more tagging options and work generically for registry
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-#Publish Docker Container To GitHub Package Registry
+#Publish Docker Container To Registry
 ####################################################
 
 # exit when any command fails
 set -e
 
 #check inputs
+if [[ -z "$INPUT_REGISTRY" ]]; then
+    echo "Set the REGISTRY input."
+    exit 1
+fi
+
 if [[ -z "$INPUT_USERNAME" ]]; then
 	echo "Set the USERNAME input."
 	exit 1
@@ -22,26 +27,29 @@ if [[ -z "$INPUT_IMAGE_NAME" ]]; then
 	exit 1
 fi
 
+if [[ -z "$INPUT_BUILD_CONTEXT" ]]; then
+    echo "Set the BUILD_CONTEXT input."
+    exit 1
+fi
+
 if [[ -z "$INPUT_DOCKERFILE_PATH" ]]; then
 	echo "Set the DOCKERFILE_PATH input."
 	exit 1
 fi
 
-if [[ -z "$INPUT_BUILD_CONTEXT" ]]; then
-	echo "Set the BUILD_CONTEXT input."
-	exit 1
-fi
-
-
-# The following environment variables will be provided by the environment automatically: GITHUB_REPOSITORY, GITHUB_SHA
+# The following environment variables will be provided by the environment automatically: GITHUB_REPOSITORY, GITHUB_SHA, GITHUB_REF
 
 # send credentials through stdin (it is more secure)
-echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin docker.pkg.github.com
+echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin ${INPUT_REGISTRY}
 
-# Set Local Variables
-shortSHA=$(echo "${GITHUB_SHA}" | cut -c1-12)
-BASE_NAME="docker.pkg.github.com/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}"
-SHA_NAME="${BASE_NAME}:${shortSHA}"
+if [ "${INPUT_REGISTRY}" == "docker.pkg.github.com" ]; then
+    # try to pull container if exists
+    BASE_NAME="${INPUT_REGISTRY}/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}"
+else
+    BASE_NAME="${INPUT_REGISTRY}/${INPUT_USERNAME}/${INPUT_IMAGE_NAME}"
+fi
+
+echo "BASE_NAME set to ${BASE_NAME}"
 
 # Add Arguments For Caching
 BUILDPARAMS=""
@@ -54,11 +62,44 @@ if [ "${INPUT_CACHE}" == "true" ]; then
 fi
 
 # Build The Container
-docker build $BUILDPARAMS -t ${SHA_NAME} -t ${BASE_NAME} -f ${INPUT_DOCKERFILE_PATH} ${INPUT_BUILD_CONTEXT}
+docker build $BUILDPARAMS --tag ${BASE_NAME} --file ${INPUT_DOCKERFILE_PATH} ${INPUT_BUILD_CONTEXT}
 
-# Push two versions, with and without the SHA
-docker push ${BASE_NAME}
-docker push ${SHA_NAME}
+if [ "${INPUT_TAG_REF}" == "true" ]; then
+    echo "tagging as ${BASE_NAME}:${GITHUB_REF}"
+    docker tag ${BASE_NAME} ${BASE_NAME}:${GITHUB_REF}
+    docker push ${BASE_NAME}:${GITHUB_REF}
+fi
 
-echo "::set-output name=IMAGE_SHA_NAME::${SHA_NAME}"
-echo "::set-output name=IMAGE_URL::https://github.com/${GITHUB_REPOSITORY}/packages"
+if [ "${INPUT_TAG_LATEST}" == "true" ]; then
+    echo "tagging as ${BASE_NAME}:latest"
+    docker tag ${BASE_NAME} "${BASE_NAME}:latest"
+    docker push "${BASE_NAME}:latest"
+fi
+
+if [ "${INPUT_TAG_VERSION}" == "true" ]; then
+    VERSION_TAG=$(docker inspect ${BASE_NAME} | jq -r .[0].ContainerConfig.Labels.version) 
+    
+    if [ "${VERSION_TAG}" != "null" ]; then
+        echo "tagging as ${BASE_NAME}:${VERSION_TAG}"
+        docker tag ${BASE_NAME} "${BASE_NAME}:${VERSION_TAG}"
+        docker push "${BASE_NAME}:${VERSION_TAG}"
+    fi
+fi
+
+if [ "${INPUT_TAG_SHA}" == "true" ]; then
+    # Set Local Variables
+    SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-12)
+    echo "tagging as ${BASE_NAME}:${SHORT_SHA}"
+    docker tag ${BASE_NAME} "${BASE_NAME}:${SHORT_SHA}"
+    docker push "${BASE_NAME}:${shortSHA}"
+fi
+
+if [ "${INPUT_TAG_TIMESTAMP}" == "true" ]; then
+    # Set Local Variables
+    TIMESTAMP_TAG=$(date +%Y-%m-%d_%HH%MM%SS%N)
+    echo "tagging as ${BASE_NAME}:${TIMESTAMP_TAG}"
+    docker tag ${BASE_NAME} "${BASE_NAME}:${TIMESTAMP_TAG}"
+    docker push "${BASE_NAME}:${TIMESTAMP_TAG}"
+fi
+
+docker logout
